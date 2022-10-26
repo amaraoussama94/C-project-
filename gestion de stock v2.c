0715f9613e61a3dd8f02079a77b44d7be1981c6e
oussama.amara <wassa400@gmail.com>
2022-10-26 09:54:23 +0100

    clean up code

--- gestion de stock v2.c
+++ gestion de stock v2.c
@@ -2,6 +2,9 @@
 #include<stdlib.h>
 #include<string.h>
 #include<ctype.h>
+
+
+
 struct prod
 {
 	char Nom_de_produit[80];
@@ -12,37 +15,11 @@ struct prod
  } ;
 
  
-///////////////////////////////////////////////
-// derniere modification:16/03/2017         //
-// fonction  recherchhe produit par  nom   //
-////////////////////////////////////////////
 
-void  RechPNom(FILE *fichier, int TailleStructure,struct prod *PointeurProduit)
-{char nom[80];
-  int trouve = 1;
-
-  printf("Donnez le nom : ");
-  scanf("%s",nom);
-  fseek(fichier, 0, SEEK_SET);
-  while (fread(PointeurProduit, TailleStructure, 1, fichier) != 0 && trouve != 0)
-      {
-       if ((trouve = strcmp(PointeurProduit->Nom_de_produit,nom)) == 0)
-
- printf("le nom de produit :%s \n Nombre de stock:%d \n Prix d achat :%f \n Prix de vente:%f \n Numero de produit:%d ",PointeurProduit->Nom_de_produit,PointeurProduit->Nombre_Stock,PointeurProduit->Prix_Achat,PointeurProduit->Prix_Vente,PointeurProduit->Num_Prod);
-       getch();
-      }
-  if (trouve != 0) {
-    printf("Ce nom n'existe pas\n");
-    getch();
-  }
-}
-
-}
 //////////////////////////////////////
 // derniere modification:10/03/2017 //
 // fonction pricipale               //
 /////////////////////////////////////
-
 main()
 {
 FILE *fichier;
